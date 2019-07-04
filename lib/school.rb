aaa042f59a0af8baa6b9a78edbee20dee4b7495e
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-04 04:54:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,9 @@ class School
   school = School.new("Bayside High School")
   
   school.add_student("Zach Morris", 9)
+  school.add_student("AC Slater", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
   school.roster
   
   hash["new_key"] = []
